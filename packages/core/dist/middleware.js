c3d6713d17a09e9c94bb61fbef31baf1cf05287c
Mark McIntosh <mmcintosh@infowall.com>
2026-01-09 13:48:08 -0500

    fix: remove setInterval from global scope (Workers violation)

--- packages/core/dist/middleware.js
+++
@@ -1,7 +0,0 @@
-export { AuthManager, PermissionManager, bootstrapMiddleware, cacheHeaders, compressionMiddleware, detailedLoggingMiddleware, getActivePlugins, isPluginActive, logActivity, loggingMiddleware, metricsMiddleware, optionalAuth, performanceLoggingMiddleware, requestTimeout, requireActivePlugin, requireActivePlugins, requireAnyPermission, requireAuth, requirePermission, requireRole, securityHeaders, securityLoggingMiddleware, withTimeout } from './chunk-IHRAULJU.js';
-import './chunk-SGAG6FD3.js';
-import './chunk-EYFPQNUW.js';
-import './chunk-FICTAGD4.js';
-import './chunk-V4OQ3NZ2.js';
-//# sourceMappingURL=middleware.js.map
-//# sourceMappingURL=middleware.js.map
--- packages/core/dist/migrations-SF6CHHE4.js
+++
@@ -1,4 +0,0 @@
-export { MigrationService } from './chunk-EYFPQNUW.js';
-import './chunk-V4OQ3NZ2.js';
-//# sourceMappingURL=migrations-SF6CHHE4.js.map
-//# sourceMappingURL=migrations-SF6CHHE4.js.map
